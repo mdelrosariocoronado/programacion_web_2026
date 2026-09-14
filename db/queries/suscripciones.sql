e228633a3715364d39abeb070215d10e3175078b
mdelrosariocoronado <mdelrosariocoronado@gmail.com>
2026-09-14 13:31:11 -0300

    adicion del getSuscripcionBYUSerAndEMprendimiento y el UpdateSuscripcion

--- db/queries/suscripciones.sql
+++ db/queries/suscripciones.sql
@@ -3,11 +3,20 @@ INSERT INTO suscripciones (id_usuario, id_emprendimiento)
 VALUES ($1, $2)
 RETURNING id_suscripcion, id_usuario, id_emprendimiento, fecha_sub;
 
+-- name: GetSuscripcionByUserAndEmprendimiento :one
+SELECT id_suscripcion, id_usuario, id_emprendimiento, fecha_sub
+FROM suscripciones
+WHERE id_usuario = $1 AND id_emprendimiento = $2;
+
 -- name: ListSuscripcionesByUsuario :many
 SELECT id_suscripcion, id_usuario, id_emprendimiento, fecha_sub
 FROM suscripciones
 WHERE id_usuario = $1;
 
+-- name: UpdateSuscripcion :exec
+UPDATE suscripciones
+SET fecha_sub = $3
+WHERE id_usuario = $1 AND id_emprendimiento = $2;
 -- name: DeleteSuscripcion :exec
 DELETE FROM suscripciones
 WHERE id_usuario = $1 AND id_emprendimiento = $2;
